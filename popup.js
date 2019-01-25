6ec48580b6957430962181675991c84803486aa9
Konstantin <rondo.devil@gmail.com>
2019-01-25 17:37:57 +0300

    Привел к более-менее божескому виду. Обработка ошибок

--- popup.js
+++ popup.js
@@ -1,17 +1,27 @@
-window.onload=function(){
-	
-	var storage = chrome.storage.sync;	
-	var FORMUL;
-	var POSTS_C;
-	var forml_inp=document.getElementById('FORML');
-	var count_inp=document.getElementById('POSTS');
-	var but = document.getElementById('BUTTON');
-	
-	storage.get("FORMULA",function(it){FORMUL=it.FORMULA; forml_inp.value=FORMUL;});
-	storage.get("POSTS",function(it){POSTS_C=it.POSTS;count_inp.value=POSTS_C;});	
-	
-	but.addEventListener('click', function() {
-        storage.set({"FORMULA" : forml_inp.value}, function() {});
-        storage.set({"POSTS" : count_inp.value}, function() {});
-    });
-}
+var chrome = window.chrome || {};
+window.addEventListener("load", () => {
+	const storage = chrome.storage.sync;
+	var conduction = "100 * (likes + 1.5 * reposts + 2 * comments ) / views";
+	var max_posts = 100;
+
+	var conduction_inp = document.getElementById("FORML");
+	var count_inp = document.getElementById("POSTS");
+
+	storage.get(["FORMULA"], it => {
+		conduction = it.FORMULA || conduction;
+		conduction_inp.value = conduction;
+	});
+
+	storage.get(["POSTS"], it => {
+		max_posts = it.POSTS || max_posts;
+		count_inp.value = max_posts;
+	});
+
+	document.querySelector("#BUTTON").addEventListener("click", () => {
+		conduction = conduction_inp.value || conduction;
+		max_posts = count_inp.value || max_posts;
+
+		storage.set({ FORMULA: conduction }, function() {});
+		storage.set({ POSTS: max_posts }, function() {});
+	});
+});
--- restApi.js
+++ restApi.js
@@ -1,42 +1,74 @@
 var VKREST = (function() {
 	const VK_REST = "https://api.vk.com/method/";
-	var TOKEN = "";
-	var APP_ID = "";
+    let TOKENS = [];
+    let currentToken = 0;
+    let startToken = 0;
 
-	function Init(app, token) {
-		TOKEN = token;
-		APP_ID = app;
-	}
+    function getCurrentToken() {
+        return TOKENS[currentToken % TOKENS.length];
+    }
+
+    function getNextToken() {
+        currentToken ++;
+        return getCurrentToken();
+    }
+
+	function Init(tokens) {
+		TOKENS = tokens;
+    }
+    
+    function createXHR(req, res, rej) {
+        const xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", r => {
+            if(!xhr.response.error)
+                res(xhr.response);
+            else{
+                rej(xhr.response);
+            }
+        });
+        xhr.addEventListener("error", rej);
+
+        const data = `${VK_REST}${req}&access_token=${getCurrentToken()}&v=5.92`;
+        xhr.open("GET", data, true);
+        xhr.responseType = "json";
+        xhr.send();
+    }
+
+    function trySend(req, res, rej) {
+        createXHR(req, res, (r) =>{
+            const delta = currentToken - startToken;
+            if(r.error.error_code == 29 && delta < TOKENS.length) {
+                console.log("TOKEN INVALID:", getCurrentToken());
+                
+                getNextToken();
+                console.log("Try next:", getCurrentToken());
+                trySend(req, res, rej);
+            } else {
+                rej(r);
+            }
+        });
+    }
 
 	function sendRequest(req) {
-		return new Promise((res, rej) => {
-			const xhr = new XMLHttpRequest();
-
-			xhr.addEventListener("load", r => {
-				res(xhr.response);
-			});
-			xhr.addEventListener("error", rej);
-
-			const data = `${VK_REST}${req}&access_token=${TOKEN}&v=5.92`;
-			xhr.open("GET", data, true);
-			xhr.responseType = "json";
-			xhr.send();
+        
+        if (!TOKENS || TOKENS.length == 0) {
+			throw Error("Api can't inited!");
+        }
+        
+        startToken = currentToken;
+        return new Promise((res, rej) =>{
+            trySend(req, res, rej)
 		});
 	}
 
 	function WallGet({ owner_id, count, extended = 0, offset = 0 }) {
-		if (!TOKEN || !APP_ID) {
-			throw Error("Api cant inited!");
-		}
 		 return sendRequest(
 			`wall.get?owner_id=${owner_id}&count=${count || 100}&extended=${extended}&offset=${offset}`
 		);
 	}
 
 	function GroupsGetById(id) {
-		if (!TOKEN || !APP_ID) {
-			throw Error("Api cant inited!");
-		}
 		return sendRequest(`groups.getById?group_id=${id}`);
 	}
 
